dd7f4b9cf08fbd7b4c6fe8bf5cefca1ec986e90c
Guy Aston <guy88a@gmail.com>
2023-06-04 00:53:01 +0300

    mobile support and updates

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -5,19 +5,21 @@ import BlogListItem from './BlogListItem';
 
 const BlogList = (props: BlogListProps) => {
     return (
-        <div className="space__page--blog__blog-list">
-            {
-                props.blogData.map((blogItem, index) => {
-                    return <BlogListItem
-                    key={`blogItem_${index}`}
-                    itemId={blogItem.itemId}
-                    title={blogItem.title}
-                    author={blogItem.author}
-                    date={blogItem.date}
-                    text={blogItem.text}
-                    delay={100 * index} />
-                })
-            }
+        <div className="blog-list-wrapper">
+            <div className="space__page--blog__blog-list">
+                {
+                    props.blogData.map((blogItem, index) => {
+                        return <BlogListItem
+                        key={`blogItem_${index}`}
+                        itemId={blogItem.itemId}
+                        title={blogItem.title}
+                        author={blogItem.author}
+                        date={blogItem.date}
+                        text={blogItem.text}
+                        delay={100 * index} />
+                    })
+                }
+            </div>
         </div>
     )
 }
